d4655a95e51ec5515724b1c9c877b571ecc4361a
gagahsyuja <gghssa1@gmail.com>
2024-03-07 21:39:28 +0700

    Add colors and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ export default {
   content: ["./index.html",
   "./src/**/*.{svelte,js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            green: "#00bf63",
+            cyan: "#0cc0df",
+            blue: "#004aad",
+            black: "#545454",
+            gray: "#d9d9d9",
+            red: "#ff3131",
+            yellow: "#ffde59"
+        },
+        fontFamily: {
+            pixel: "Pixelify\\ Sans"
+        }
+    },
   },
   plugins: [],
 }
