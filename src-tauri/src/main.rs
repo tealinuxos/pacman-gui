f6bc37364d97e98b94644c8b8e7d0de6ce124b36
Harisdinus <111202113618@mhs.dinus.ac.id>
2024-03-09 11:05:19 +0700

    Cara menjalankan menggunakan akses sudo

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,9 +7,17 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+use users::get_current_uid;
+
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    let user = get_current_uid();
+
+    if user == 0 {
+        tauri::Builder::default()
+            .invoke_handler(tauri::generate_handler![greet])
+            .run(tauri::generate_context!())
+            .expect("error while running tauri application");
+    } else {
+        println!("Kwe ora root jancok");
+    }
 }
