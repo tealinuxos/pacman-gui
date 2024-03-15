3f9e508439c0ad53b016a63fb60d930e0c816093
LiXuang12 <muhfarisassami@gmail.com>
2024-03-15 21:47:06 +0700

    memperbaiki erorr dengan mengganti enable mwnjadi disable

--- src/InstallButton.svelte
+++ src/InstallButton.svelte
@@ -9,19 +9,19 @@
 </script>
 
 {#if isDone}
-    <button enabled={false} class="bg-blue px-16 py-3 rounded-full text-white text-3xl uppercase">
+    <button disabled={true} class="bg-blue px-16 py-3 rounded-full text-white text-3xl uppercase">
         <slot />
     </button>
 {:else if isInstalling}
-    <button enabled={false} class="bg-gray px-16 py-3 rounded-full text-black text-3xl uppercase">
+    <button disabled={true} class="bg-gray px-16 py-3 rounded-full text-black text-3xl uppercase">
         {installMessage}
     </button>
 {:else if isTicked}
-    <button enabled={true} on:click={installPackage} class="bg-green px-16 py-3 rounded-full text-white text-3xl uppercase">
+    <button disabled={false} on:click={installPackage} class="bg-green px-16 py-3 rounded-full text-white text-3xl uppercase">
         <slot />
     </button>
 {:else}
-    <button enabled={false} class="bg-gray px-16 py-3 rounded-full text-black text-3xl uppercase">
+    <button disabled={true} class="bg-gray px-16 py-3 rounded-full text-black text-3xl uppercase">
         <slot />
     </button>
 {/if}
